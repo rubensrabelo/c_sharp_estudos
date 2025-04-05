5690eeae66ea3cda51373f8c4e2f43758c419831
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-05 07:03:09 -0300

    Product program

---
+++ nelio_alves_c_sharp_completo/04_intro_class/02_clr_product/Product.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Globalization;
+
+namespace Course {
+    public class Product {
+        private string Name { get; set; }
+        private double Price { get; set;}
+        private int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValueInStock() {
+            return Price * Quantity;
+        }
+
+        public void AddProduct(int quantity) {
+            this.Quantity += quantity;
+        }
+
+        public void RemoveProduct(int quantity) {
+            this.Quantity -= quantity;
+        }
+
+        public override string ToString() {
+            return "Name = " + Name +
+                ", Price = $" + Price.ToString("F2", CultureInfo.InvariantCulture) +
+                ", Quantity = " + Quantity +
+                ", Total value in Stock = " + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ nelio_alves_c_sharp_completo/04_intro_class/02_clr_product/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter product's name:");
+            string name = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            Console.WriteLine("Enter product's price: ");
+            double price = double.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter product's quantity: ");
+            int quantity = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+            Product prod = new Product(name, price, quantity);
+
+            Console.WriteLine(prod + "\n");
+
+            Console.WriteLine("Add quantity in the product: ");
+            quantity = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+            prod.AddProduct(quantity);
+
+            Console.WriteLine(prod + "\n");
+
+            Console.WriteLine("Remove quantity in the product: ");
+            quantity = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+            prod.RemoveProduct(quantity);
+
+            Console.WriteLine(prod + "\n");
+        }
+    }
+} 
