fa1838bc88adcb3d2b6222091e2e8908371cc1e2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-26 09:12:07 -0300

    feat(worker): finish worker exercise

--- nelio_alves_c_sharp_completo/08_enum_comp/worker/Program.cs
+++ nelio_alves_c_sharp_completo/08_enum_comp/worker/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using Course.Entity;
+using Course.Entity.enums;
+
+namespace Course {
+    public class Program {
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Enter department's name:");
+            string name = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            Departament departament = new Departament(name);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter worker data:");
+            Console.WriteLine("Name:");
+            name = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            Console.WriteLine("Level (Junior/MidLevel/Senior): ");
+            string level = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            WorkerLevel levelEnum = (WorkerLevel) Enum.Parse(typeof(WorkerLevel), level);
+
+            Console.WriteLine("Base salary:");
+            double baseSalary = double.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+            Worker worker = new Worker(name, levelEnum, baseSalary, departament);
+
+            Console.WriteLine();
+            Console.WriteLine("How many contracts to this worker?");
+            int n = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException());
+
+            for(int i=0; i<n; i++) {
+                Console.WriteLine($"Enter #{i+1} contract data:");
+                Console.WriteLine("Date (DD/MM/YYYY):");
+                string dateStr = Console.ReadLine() ?? throw new ArgumentNullException();
+
+                DateTime date = DateTime.ParseExact(dateStr, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Value per hour:");
+                double valuePerHour = double.Parse(Console.ReadLine() ?? throw new ArgumentNullException(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Duration(hours):");
+                int hour = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException());
+
+                HourContract hourContract = new HourContract(date, valuePerHour, hour);
+
+                worker.AddContract(hourContract);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter month and year to calculate income (MM/YYYY):");
+            String monthYear = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            String[] parts = monthYear.Split("/");
+
+            int month = int.Parse(parts[0]);
+            int year = int.Parse(parts[1]);
+
+            double valueIncome = worker.Income(month, year);
+
+            Console.WriteLine();
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Departament: {worker.Departament}");
+            Console.WriteLine($"Income for {monthYear}: {valueIncome}");
+        }
+    }
+}
--- nelio_alves_c_sharp_completo/08_enum_comp/worker/entity/Worker.cs
+++ nelio_alves_c_sharp_completo/08_enum_comp/worker/entity/Worker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using Course.Entity.enums;
 
 namespace Course.Entity {
@@ -10,14 +11,32 @@ namespace Course.Entity {
 
         public List<HourContract> Contracts { get; private set; } = new List<HourContract>();
 
-        public Worker() {
-        } 
+        public Worker() {} 
 
         public Worker(String name, WorkerLevel level, double baseSalary, Departament departament) {
             Name = name;
             Level = level;
             BaseSalary = baseSalary;
             Departament = departament;
+        }
+
+        public void AddContract(HourContract contract) {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract) {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int month, int year) {
+            double valueTotal = BaseSalary;
+
+            foreach(HourContract contract in Contracts) {
+                if(contract.Date.Year == year && contract.Date.Month == month) 
+                    valueTotal += contract.TotalValue();
+            }
+            
+            return valueTotal;
         }
     }
 }
