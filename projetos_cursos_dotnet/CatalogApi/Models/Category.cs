b184ab4e80c4e18bd4ef2ba49c2ff8d5aa57b078
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-05-23 16:01:10 -0300

    feat(order): add initial structure in the project

---
+++ projetos_cursos_dotnet/CatalogApi/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace Category.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? I { get; set; }
+
+    }
+}
